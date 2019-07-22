5c5e6bb7f0bb286b361489e9b12391b2010d98b0
omer21-meet <omer21@meet.mit.edu>
2019-07-22 16:39:35 +0300

    Stopping here

---
+++ Day2/newfolder/shell2.py
@@ -0,0 +1,28 @@
+Python 3.6.8 (default, Jan 14 2019, 11:02:34) 
+[GCC 8.0.1 20180414 (experimental) [trunk revision 259383]] on linux
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=5
+>>> b=10
+>>> a=b
+>>> b=a
+>>> type(a)
+<class 'int'>
+>>> a
+10
+>>> b
+10
+>>> a=5
+>>> b=10
+>>> a=b
+>>> a
+10
+>>> b
+10
+>>> c=a
+>>> a=b
+>>> b=c
+
+>>> four='4'
+>>> print(four*3)
+444
+>>>  
