9b59ee635f0c52bfde2d6dc8b5e8bad9435c295c
omer21-meet <omer21@meet.mit.edu>
2019-07-22 15:56:56 +0300

    Stopping here

--- Day1/newFolder/meetinturtle1.py
+++ Day1/newFolder/meetinturtle1.py
@@ -1,7 +1,7 @@
 import turtle
 
 turtle.penup()
-turtle.speed(500
+turtle.speed(500)
 turtle.goto(-800,0)
 turtle.pendown()
 turtle.left(90)
@@ -38,6 +38,7 @@ turtle.left(90)
 turtle.pendown()
 turtle.forward(400)
 turtle.right(90)
+
 turtle.forward(200)
 turtle.back(200)
 turtle.right(90)
@@ -63,4 +64,10 @@ turtle.forward(100)
 turtle.right(90)
 turtle.pendown()
 turtle.forward(300)
+turtle.penup()
+turtle.forward(30)
+turtle.right(90)
+
+turtle.pendown()
+turtle.circle(50)
 turtle.mainloop()
